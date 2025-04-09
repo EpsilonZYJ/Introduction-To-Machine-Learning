9bcb21fb280a4fbef4345d14d043c5244117e35e
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2025-04-10 00:30:22 +0800

    Experiement4

---
+++ Experiement/kNN/Task2.py
@@ -0,0 +1,17 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+def classification(train_feature, train_label, test_feature):
+    '''
+    使用KNeighborsClassifier对test_feature进行分类
+    :param train_feature: 训练集数据
+    :param train_label: 训练集标签
+    :param test_feature: 测试集数据
+    :return: 测试集预测结果
+    '''
+
+    #********* Begin *********#
+    model = KNeighborsClassifier()
+    model.fit(train_feature, train_label)
+    result = model.predict(test_feature)
+    return result
+    #********* End *********#
